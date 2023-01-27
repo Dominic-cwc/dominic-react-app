69c1affcbe28ab36797ae4e29d1fba24dbf3c50d
Dominic Chow <dominicchow83@yahoo.com.hk>
2023-01-27 21:30:09 +0800

    123

--- dominic-react-app/src/index.js
+++ dominic-react-app/src/index.js
@@ -1,4 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { Teacher } from './classes(11-13)/teacher';
+import './css/style.css'
+
 
 const teacher = new Teacher("Dominic", "Masters");
 teacher.teach();
+
+const colors = ['red', 'green', 'blue'];
+const items = colors.map(color => <li>{color}</li>); //using `` can map those the html tag together?
+
+ReactDOM.render(<ol>{items}</ol>, document.getElementById('root'));
