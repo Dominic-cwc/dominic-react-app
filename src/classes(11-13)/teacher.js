8d0ffc86941753e50945570b22438ea39d70bd6d
Dominic <dominicchow83@yahoo.com.hk>
2023-01-30 13:06:48 +0800

    123

--- src/classes(11-13)/teacher.js
+++ src/classes(11-13)/teacher.js
@@ -1,6 +1,10 @@
 import {Person} from './person';
 
-export class Teacher extends Person {
+function promote(){ return 1;}
+export default promote;
+
+
+export default class Teacher extends Person {
     constructor(name, degree) {
         super(name);
         this.degree = degree;
